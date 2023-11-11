0b967156d710e1bfab7e7de07f7b61fc38160e50
Markus Rösner <markus.roesner@gmx.net>
2023-11-11 10:23:27 +0100

    fixed prettier/prettier problems

--- src/ui/reminder.ts
+++ src/ui/reminder.ts
@@ -1,46 +1,50 @@
-import type { ReadOnlyReference } from "model/ref";
-import type { DateTime } from "model/time";
-import { App, Modal } from "obsidian";
-import { SETTINGS } from "settings";
-import type { Reminder } from "../model/reminder";
-import type { Later } from "../model/time";
-import ReminderView from "./components/Reminder.svelte";
-const electron = require("electron");
+import type { ReadOnlyReference } from 'model/ref';
+import type { DateTime } from 'model/time';
+import { App, Modal } from 'obsidian';
+import { SETTINGS } from 'settings';
+import type { Reminder } from '../model/reminder';
+import type { Later } from '../model/time';
+import ReminderView from './components/Reminder.svelte';
+const electron = require('electron');
 
 export class ReminderModal {
-
-  constructor(private app: App, private usePhonePushNotifications: ReadOnlyReference<boolean>, private ntfyTopic: ReadOnlyReference<string>, private useSystemNotification: ReadOnlyReference<boolean>, private laters: ReadOnlyReference<Array<Later>>) { }
+  constructor(
+    private app: App,
+    private usePhonePushNotifications: ReadOnlyReference<boolean>,
+    private ntfyTopic: ReadOnlyReference<string>,
+    private useSystemNotification: ReadOnlyReference<boolean>,
+    private laters: ReadOnlyReference<Array<Later>>,
+  ) {}
 
   public show(
     reminder: Reminder,
     onRemindMeLater: (time: DateTime) => void,
     onDone: () => void,
     onMute: () => void,
-    onOpenFile: () => void
+    onOpenFile: () => void,
   ) {
-
-    console.log(this.app.vault)
+    console.log(this.app.vault);
 
     if (this.usePhonePushNotifications.value) {
       const xhr = new XMLHttpRequest();
-      xhr.open("POST", "https://ntfy.sh/", true);
-      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.open('POST', 'https://ntfy.sh/', true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
 
       xhr.onreadystatechange = () => {
         if (xhr.readyState === XMLHttpRequest.DONE) {
           if (xhr.status === 200) {
-            console.log("Notification sent successfully");
+            console.log('Notification sent successfully');
           } else {
-            console.error("Failed to send notification", xhr.statusText);
+            console.error('Failed to send notification', xhr.statusText);
           }
         }
       };
 
-      let data = JSON.stringify({
-        "topic": this.ntfyTopic.value,
-        "title": reminder.title,
-        "message": "Reminder: " + reminder.title,
-        "click": `obsidian://open?vault=${this.app.vault.getName()}&file=${encodeURIComponent(reminder.file)}`
+      const data = JSON.stringify({
+        topic: this.ntfyTopic.value,
+        title: reminder.title,
+        message: 'Reminder: ' + reminder.title,
+        click: `obsidian://open?vault=${this.app.vault.getName()}&file=${encodeURIComponent(reminder.file)}`,
       });
 
       xhr.send(data);
@@ -52,22 +56,21 @@ export class ReminderModal {
       // Show system notification
       const Notification = (electron as any).remote.Notification;
       const n = new Notification({
-        title: "Obsidian Reminder",
+        title: 'Obsidian Reminder',
         body: reminder.title,
       });
 
-
-      n.on("click", () => {
+      n.on('click', () => {
         n.close();
         this.showBuiltinReminder(reminder, onRemindMeLater, onDone, onMute, onOpenFile);
       });
-      n.on("close", () => {
+      n.on('close', () => {
         onMute();
       });
       // Only for macOS
       {
         const laters = SETTINGS.laters.value;
-        n.on("action", (_: any, index: any) => {
+        n.on('action', (_: any, index: any) => {
           if (index === 0) {
             onDone();
             return;
@@ -75,9 +78,9 @@ export class ReminderModal {
           const later = laters[index - 1]!;
           onRemindMeLater(later.later());
         });
-        const actions = [{ type: "button", text: "Mark as Done" }];
-        laters.forEach(later => {
-          actions.push({ type: "button", text: later.label })
+        const actions = [{ type: 'button', text: 'Mark as Done' }];
+        laters.forEach((later) => {
+          actions.push({ type: 'button', text: later.label });
         });
         n.actions = actions as any;
       }
@@ -91,7 +94,7 @@ export class ReminderModal {
     onRemindMeLater: (time: DateTime) => void,
     onDone: () => void,
     onCancel: () => void,
-    onOpenFile: () => void
+    onOpenFile: () => void,
   ) {
     new NotificationModal(this.app, this.laters.value, reminder, onRemindMeLater, onDone, onCancel, onOpenFile).open();
   }
@@ -106,12 +109,9 @@ export class ReminderModal {
   private isMobile() {
     return electron === undefined;
   }
-
-
 }
 
 class NotificationModal extends Modal {
-
   canceled: boolean = true;
 
   constructor(
@@ -121,7 +121,7 @@ class NotificationModal extends Modal {
     private onRemindMeLater: (time: DateTime) => void,
     private onDone: () => void,
     private onCancel: () => void,
-    private onOpenFile: () => void
+    private onOpenFile: () => void,
   ) {
     super(app);
   }
@@ -131,7 +131,7 @@ class NotificationModal extends Modal {
     // lets us introspect the reminder's display state from elsewhere.
     this.reminder.beingDisplayed = true;
 
-    let { contentEl } = this;
+    const { contentEl } = this;
     new ReminderView({
       target: contentEl,
       props: {
@@ -165,7 +165,7 @@ class NotificationModal extends Modal {
     // Unset the reminder from being displayed. This lets other parts of the
     // plugin continue.
     this.reminder.beingDisplayed = false;
-    let { contentEl } = this;
+    const { contentEl } = this;
     contentEl.empty();
     if (this.canceled) {
       this.onCancel();
